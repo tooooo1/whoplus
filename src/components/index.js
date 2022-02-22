07d28f463553f480ca75d4870c606b9732ef63c6
tooooo1 <wer4272@nate.com>
2022-02-23 02:36:01 +0900

    delete : 불필요한 요소 제거

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as NumberWrapper } from './NumberWrapper';
-export { default as NumberMain } from './NumberMain';
-export { default as NumberFunc } from './NumberFunc';
-export { default as NumberEnd } from './NumberEnd';
-export { default as AgainButton } from './AgainButton';
-export { default as NumberStart } from './NumberStart';
