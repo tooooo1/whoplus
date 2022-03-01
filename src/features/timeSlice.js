8cdfc08ec280242eb0d02a1609d847320ad3fcee
tooooo1 <wer4272@nate.com>
2022-03-01 19:17:40 +0900

    update : 모드별 시간 정의

--- src/features/timeSlice.js
+++ src/features/timeSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = { value: 2 }
+const initialState = { value: 2, dementia: 4 }
 
 export const timeSlice = createSlice({
   name: 'time',
@@ -9,12 +9,16 @@ export const timeSlice = createSlice({
     incrementTime: (state) => {
       state.value += 2
     },
+    incrementTimeDementia: (state) => {
+      state.dementia += 4
+    },
     timeReset: (state) => {
       state.value = 2
-    }
+      state.dementia = 4
+    },
   },
 });
 
-export const { incrementTime, timeReset } = timeSlice.actions;
+export const { incrementTime, timeReset, incrementTimeDementia } = timeSlice.actions;
 
 export default timeSlice.reducer;
