f3b6419c079c9ee617f6b3ffe04df56a0e9cf0bf
tooooo1 <wer4272@nate.com>
2022-03-02 23:41:48 +0900

    style : 버전별 시간 로직 적용법 변경

--- src/features/timeSlice.js
+++ src/features/timeSlice.js
@@ -1,24 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = { value: 2, dementia: 4 }
+const initialState = { value: 2, plus: 0 }
 
 export const timeSlice = createSlice({
   name: 'time',
   initialState,
   reducers: {
     incrementTime: (state) => {
-      state.value += 2
-    },
-    incrementTimeDementia: (state) => {
-      state.dementia += 4
+      state.value += state.plus;
     },
     timeReset: (state) => {
       state.value = 2
-      state.dementia = 4
     },
+    setValue: (state, actions) => {
+      state.value = actions.payload;
+    },
+    setPlus: (state, actions) => {
+      state.plus = actions.payload;
+    }
   },
 });
 
-export const { incrementTime, timeReset, incrementTimeDementia } = timeSlice.actions;
+export const { incrementTime, timeReset, setValue, setPlus } = timeSlice.actions;
 
 export default timeSlice.reducer;
