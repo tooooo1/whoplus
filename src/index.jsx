1e7ce8c709901225836c98bc446eec4a0e297415
tooooo1 <wer4272@nate.com>
2022-12-01 23:29:24 +0900

    refactor: index.js remove reportwebvitals

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { store } from './app/store'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import App from './App.jsx';
+import { store } from './app/store.js';
+import './index.css';
 
 let persistor = persistStore(store);
 
@@ -20,5 +19,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-reportWebVitals();
