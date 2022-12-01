f557e5e661f6dae5db5c6ca0cdfd8283fd2d4a36
tooooo1 <wer4272@nate.com>
2022-12-01 22:56:13 +0900

    remove: Input component

--- src/components/Input/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import * as Styled from './styled';
-
-const Input = ({ onChange, placeholder }) => (
-    <Styled.Input onChange={onChange} placeholder={placeholder} />
-);
-
-export default Input;
