2e79e70d3e48387105a4c70d493028cfcfba6a82
tooooo1 <wer4272@nate.com>
2022-12-01 23:57:14 +0900

    remove: redux-persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,32 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
-import roundReducer from '../features/roundSlice.js';
-import timeReducer from '../features/timeSlice.js';
+
 import nicknameReducer from '../features/nicknameSlice.js';
 import powerReducer from '../features/powerSlice.js';
 import resultReducer from '../features/resultSlice.js';
-
-import storage from 'redux-persist/lib/storage';
-import { combineReducers } from 'redux';
-import { persistReducer } from 'redux-persist';
-import thunk from 'redux-thunk';
-
-const reducers = combineReducers({
-  round: roundReducer,
-  time: timeReducer,
-  nickname: nicknameReducer,
-  power: powerReducer,
-  result: resultReducer,
-});
-
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, reducers);
+import roundReducer from '../features/roundSlice.js';
+import timeReducer from '../features/timeSlice.js';
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  devTools: process.env.NODE_ENV !== 'production',
-  middleware: [thunk],
+  reducer: {
+    round: roundReducer,
+    time: timeReducer,
+    nickname: nicknameReducer,
+    power: powerReducer,
+    result: resultReducer,
+  },
 });
--- src/index.jsx
+++ src/index.jsx
@@ -3,20 +3,14 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { persistStore } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App.jsx';
 import { store } from './app/store.js';
 
-let persistor = persistStore(store);
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
