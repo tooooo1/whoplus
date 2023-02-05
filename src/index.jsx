3589846699131ba5877f9f65e8438bd3cd6d30d6
tooooo1 <wer4272@nate.com>
2023-02-05 23:59:23 +0900

    chore: react18 + ts

--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,10 @@ import ReactDOM from 'react-dom';
 
 import App from './App.jsx';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
