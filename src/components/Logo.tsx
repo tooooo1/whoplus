b26d6fda4fb0de5417c5b11bfd3a4467c56a764f
tooooo1 <wer4272@nate.com>
2023-09-02 12:59:11 +0900

    refactor: Improve Logo component's alt text, replace window.open with anchor, add aria-label for screen readers

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,22 +1,27 @@
 import styled from '@emotion/styled';
 
 const Logo = () => (
-  <Img
-    onClick={() => window.open('https://tooo1.tistory.com')}
-    src="images/il.jpg"
-    alt="il"
-    width={50}
-    height={50}
-  />
+  <a
+    href="https://tooo1.tistory.com"
+    target="_blank"
+    rel="noopener noreferrer"
+    aria-label="tooo1 Blog"
+  >
+    <StyledImg
+      src="images/il.jpg"
+      alt="tooo1 Blog Logo"
+      width={50}
+      height={50}
+    />
+  </a>
 );
 
 export default Logo;
 
-export const Img = styled.img`
+const StyledImg = styled.img`
   position: absolute;
   top: 10px;
   left: 10px;
   z-index: 1;
   border-radius: 100px;
-  cursor: pointer;
 `;
